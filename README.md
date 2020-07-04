c7b72a06b9f10d9527220ba749d4508d3793f2f7
Edward de Leau <e@leau.net>
2020-07-04 08:16:04 +0200

    1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # getfavicon
-Get favicon
+Parses a folderstructure with text files and downloads a favicon for each url found in them from the Google Favicon service.
+
+This can be used for pre-caching these icons.
+
+# usage
+
+pass the folder to read and the folder to write icons to, to the function "TraverseDir"
+
+# example
+
+`
+TraverseDir('c:/a/website//src/assets/data/', 'c:/a/websites/icon/');
+`
+
+or e.g. in package.json before build:
+
+`
+"scripts": {
+    "fetchicons": "run-func /src/util/favicon.js TraverseDir c:/a/website//src/assets/data/ c:/a/websites/icon/"
+  }
+`
---
+++ favicon.js
@@ -0,0 +1,63 @@
+﻿#!/usr/bin/env node
+const dir = require('node-dir');
+const util = require('util');
+const fs = require('fs');
+const readFile = util.promisify(fs.readFile);
+const fse = require('fs-extra');
+const got = require('got');
+const stream = require('stream');
+const {promisify} = require('util');
+const pipeline = promisify(stream.pipeline);
+const googleFaviconApi = 'https://s2.googleusercontent.com/s2/favicons?domain_url=';
+
+/**
+* Downloads a favion in reverse domain name folder
+*/
+function GenerateName(url, imgRootDir) {
+    const domain = url.split("/")[2]
+    const reversedomain = domain.split('.').reverse().join('.');
+    const reversedomainslash = domain.split('.').reverse().join('/');
+    const imgDir = imgRootDir + reversedomainslash;
+    const imgFilePath = imgDir + "/favicon.png";
+
+    fse.ensureDirSync(imgDir);
+    fs.open(imgFilePath, 'wx', (err, fd) => {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                return;
+            }
+            throw err;
+        }
+
+        (async () => {
+            await pipeline(
+                got.stream(googleFaviconApi + url),
+                fs.createWriteStream(imgFilePath)
+            );
+            console.log("created favion " + imgFilePath);
+        })();
+    });
+
+    return reversedomain;
+}
+
+/**
+ * Get set of urls out of text
+ */
+function linkify(rawText, imgRootDir) {
+    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return rawText.replace(urlRegex, function(url) {
+        var a = GenerateName(url, imgRootDir);
+    });
+}
+
+/**
+ * Travese a directory for text files
+ */
+function TraverseDir(dirs, imgRootDir) {
+    dir.promiseFiles(dirs)
+    .then((files)=>{files.map(f=> { var r= {};readFile(f,'utf8').then ((file) => {linkify(file, imgRootDir);})})})
+    .catch(e=>console.error(e))
+}
+
+module.exports = { TraverseDir };
